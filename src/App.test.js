d8c2a9bfce1bb2aa3cfb7f745786f409e3911f28
fora-heims <foraheims@gmail.com>
2022-01-28 08:08:30 -0800

    waitFor loading screen to go away and component to load test passing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.setTimeout(10000);
+
+test('renders fetch title after loading state goes away', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  waitFor(() => {
+    expect(screen.findByText(/so fetch/i)).toBeInTheDocument();
+  });
 });
